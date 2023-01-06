530ba6d8fce7fcca554a23176035ac2851ff74e7
0GUrEtZ <ogurtsov.i.i@mail.ru>
2023-01-07 00:45:26 +0300

    hbdfhdfg

--- src/pages/ToDo/ToDo.jsx
+++ src/pages/ToDo/ToDo.jsx
@@ -2,7 +2,99 @@ import React, { useState } from 'react';
 import t from "./ToDo.module.css";
 
 const ToDo = () => {
-    const [state, setstate] = useState(0);
+    let data = [
+        {
+            id: '1',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '2',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '3',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '4',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '5',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '6',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '7',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '8',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '9',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '10',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '11',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '12',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '13',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '14',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '15',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '16',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '17',
+            task: 'eat human heart',
+            complete: 0
+        },
+        {
+            id: '18',
+            task: 'eat human heart',
+            complete: 0
+        }
+    ];
+    const [toDoList, setToDoList] = useState(data);
     return (
         <div className={t.wrapper}>
             <div className={t.list}>
@@ -12,13 +104,16 @@ const ToDo = () => {
             </div>
             <div className={t.interaction}>
                 <input type="text" className={t.input} />
-                <button className={t.button}>
-                    add
-                </button>
-                <button className={t.button}>
-                    del
-                </button>
+                <div className={t.buttons}>
+                    <button className={t.button}>
+                        add
+                    </button>
+                    <button className={t.button}>
+                        del
+                    </button>
+                </div>
             </div>
         </div>
     )
-}; export default ToDo
+};
+export default ToDo
---
+++ src/pages/ToDo/data.js
@@ -0,0 +1,92 @@
+let arr = [
+    {
+        id: '1',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '2',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '3',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '4',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '5',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '6',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '7',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '8',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '9',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '10',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '11',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '12',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '13',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '14',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '15',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '16',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '17',
+        task: 'eat human heart',
+        complete: 0
+    },
+    {
+        id: '18',
+        task: 'eat human heart',
+        complete: 0
+    }
+]
