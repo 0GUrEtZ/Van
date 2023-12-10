76225005837ead717f81a1d539f73435a284b5e2
0GUrEtZ <ogurtsov.i.i@mail.ru>
2023-12-10 20:31:21 +0300

    v0.21

--- src/main/footer/footer.jsx
+++ src/main/footer/footer.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import f from './footer.module.css'
 import vklogo from '../../images/vk.png';
 import gitlogo from '../../images/github.png'
+import tglogo from '../../images/telegram.png'
 
 const Ftr = () => {
   return (
     <div className={f.wrapper}>
       <div className={f.links}>
-        <a href='https://github.com/0GUrEtZ/Van'><img src={gitlogo} /></a>
+        <a href='https://t.me/ogurtsovilya'><img src={tglogo} /></a>
         <a href='https://vk.com/ogurtsov_ilya'><img src={vklogo} /></a>
+        <a href='https://github.com/0GUrEtZ/Van'><img src={gitlogo} /></a>
       </div>
       <span>
         Создатель: Илья Огурцов
